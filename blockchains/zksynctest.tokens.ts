67cc92ef8d28ea62dace317ac9805371212e2a2c
Hatam <hatamimehrdadd@gmail.com>
2023-06-05 11:49:32 +0200

    add tokens

---
+++ blockchains/zksynctest.tokens.ts
@@ -0,0 +1,10 @@
+export const ZKSYNCTEST_TOKENS = [
+  {
+    chainId: 280,
+    address: '0x000000000000000000000000000000000000800A',
+    name: 'Ethereum',
+    symbol: 'ETH',
+    decimals: 18,
+    observable: true,
+  },
+];
